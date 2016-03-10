8f417fb7f4212d1bfb46735d37eeecdead1ae568
Hannes Klauser <klaushan@students.zhaw.ch>
2016-03-10 09:29:58 +0100

    removed rsync exclude switches, changed verbose to quiet again

--- deploy.sh
+++ deploy.sh
@@ -19,6 +19,7 @@ if [ ! -w "$TARGET_DIR" ]; then
 fi
 
 cd $PULL_DIR/$REPO_NAME
+# There's probably a better way to do the following
 eval `ssh-agent`
 ssh-add /home/gendok/.ssh/gendok-git
 git pull
@@ -26,7 +27,7 @@ npm install --production
 gulp deploy
 ./node_modules/.bin/sequelize db:migrate
 forever stopall
-rsync -av --exclude '.git' --exclude 'test' --exclude 'migrations' --exclude 'reports' --exclude 'models' --exclude '.gitignore' --exclude '.jscs.json' --exclude '.jshintignore' --exclude 'jshintrc' --exclude '.npmignore' --exclude '.sequelizerc' --exclude 'bower.json' --exclude 'deploy.sh' --exclude 'gulpfile.js' --exclude 'README.md' $PULL_DIR/$REPO_NAME/ $TARGET_DIR/.
+rsync -aq $PULL_DIR/$REPO_NAME/ $TARGET_DIR/.
 cd $TARGET_DIR
 GENDOK_ENV=production forever start bin/$EXECUTABLE
 exit 0
