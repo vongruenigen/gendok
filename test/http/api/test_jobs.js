57dae33cfa9c7d4284ee72071a6ea74400739e17
contrben <contrben@students.zhaw.ch>
2016-03-23 09:24:09 +0100

    added files for job-api.

---
+++ test/http/api/test_jobs.js
@@ -0,0 +1,21 @@
+/**
+ * PSIT4 FS2016 - gendok
+ *
+ * Authors: Dirk von Grünigen, Tobias Huonder, Simon Müller,
+ *          Martin Weilenmann, Aurelio Malacarne, Hannes Klauser,
+ *          Benjamin Contreras
+ *
+ */
+
+'use strict';
+
+var jobsApi = require('../../..').http.api.jobs;
+var Job = require('../../..').data.model.job;
+var Config = require('../../..').config;
+var expect = require('chai').expect;
+var helper = require('../../helper');
+var request = require('superagent');
+
+describe('gendok.http.api.jobs', function () {
+  // TODO: Implement tests for api-functions
+});
