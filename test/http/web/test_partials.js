2f10e9fe490cfe4874b84f2394cbae91f72ebc1c
Dirk von Grünigen <dirk@vongruenigen.com>
2016-04-15 22:42:07 +0200

    added the partials module for statically rendering our views

---
+++ test/http/web/test_partials.js
@@ -0,0 +1,47 @@
+/**
+ * PSIT4 FS2016 - gendok
+ *
+ * Authors: Dirk von Grünigen, Tobias Huonder, Simon Müller,
+ *          Martin Weilenmann, Aurelio Malacarne, Hannes Klauser,
+ *          Benjamin Contreras
+ *
+ */
+
+'use strict';
+
+var gendokHttp = require('../../..').http;
+var partials = gendokHttp.web.partials;
+var all = gendokHttp.middleware.all;
+var expect = require('chai').expect;
+var helper = require('../../helper');
+var request = require('superagent');
+
+describe('gendok.http.web.partials', function () {
+  it('is a function', function () {
+    expect(partials).to.be.a('function');
+  });
+
+  var server = helper.runHttpServer(this, [all, partials]);
+  var url = helper.getUrl('/partials');
+
+  describe('GET /partials/:name', function () {
+    it('returns the rendered templates as html', function (done) {
+      request.get(url + '/error')
+             .end(function (err, res) {
+               expect(err).to.not.exist;
+               expect(res.get('Content-Type')).to.include('text/html');
+               expect(res.get('Content-Length')).to.be.above(0);
+               done();
+             });
+    });
+
+    it('returns a 404 for a non existing template', function (done) {
+      request.get(url + '/blub')
+             .end(function (err, res) {
+               expect(err).to.exist;
+               expect(res.statusCode).to.eql(404);
+               done();
+             });
+    });
+  });
+});
