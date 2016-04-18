62028d02e737ba8f454d05a461697cf8433e5c9a
Hannes Klauser <klaushan@students.zhaw.ch>
2016-04-18 19:32:08 +0200

    finished implementation of email worker

--- test/queue/worker/test_email.js
+++ test/queue/worker/test_email.js
@@ -11,10 +11,19 @@
 
 var gendok = require('../../../');
 var config = require('../../../lib/config');
+var util = gendok.util;
 var email = require('../../../lib/queue/worker/email');
 var expect = require('chai').expect;
+var mockTransport = require('nodemailer-mock-transport');
 
 describe('gendok.queue.worker.email', function () {
+  var transport = null;
+
+  beforeEach(function () {
+    transport = mockTransport();
+    util.createMailer(transport, true);
+  });
+
   it('is a function', function (done) {
     expect(email).to.be.a('function');
     done();
@@ -22,10 +31,10 @@ describe('gendok.queue.worker.email', function () {
 
   describe('returns an error if there are missing options', function () {
     it('"to" is missing', function (done) {
-      var data = {
+      var data = {data: {
         subject: 'Blubb',
         html: '<b>This is an email</b>'
-      };
+      }};
 
       email(data, function (err) {
         expect(err).to.exist;
@@ -34,10 +43,10 @@ describe('gendok.queue.worker.email', function () {
     });
 
     it('"subject" is missing', function (done) {
-      var data = {
+      var data = {data: {
         to: 'blubb@gendok.ch',
         html: '<b>This is an email</b>'
-      };
+      }};
 
       email(data, function (err) {
         expect(err).to.exist;
@@ -46,10 +55,10 @@ describe('gendok.queue.worker.email', function () {
     });
 
     it('"html" is missing', function (done) {
-      var data = {
+      var data = {data: {
         to: 'blubb@gendok.ch',
         subject: 'Blubb',
-      };
+      }};
 
       email(data, function (err) {
         expect(err).to.exist;
@@ -58,11 +67,33 @@ describe('gendok.queue.worker.email', function () {
     });
   });
 
-  it('doesn\'t return an error when all parameters are set');
+  it('sends an email according to options', function (done) {
+    var data = {data: {
+      to: 'blubb@gendok.ch',
+      subject: 'Blubb',
+      html: '<b>This is an email</b>'
+    }};
 
-  it('sends an email according to options');
+    email(data, function (err) {
+      expect(err).to.not.exist;
+      expect(transport.sentMail.length).to.eql(1);
+      expect(transport.sentMail[0].data.to).to.eql(data.data.to);
+      expect(transport.sentMail[0].data.subject).to.eql(data.data.subject);
+      expect(transport.sentMail[0].message.content).to.eql(data.data.html);
+      done();
+    });
+  });
 
-  it('doesn\'t send an email if there is an error');
+  it('doesn\'t send an email if there is an error', function (done) {
+    var data = {data: {
+      to: 'blubb@gendok.ch',
+      subject: 'Blubb',
+    }};
 
-  it('uses the SMTP credentials in the config');
+    email(data, function (err) {
+      expect(err).to.exist;
+      expect(transport.sentMail.length).to.eql(0);
+      done();
+    });
+  });
 });
