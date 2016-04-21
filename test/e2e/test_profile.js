41d138ec6a8d574050ca1adbf8e38e6854801e2d
contrben <contrben@students.zhaw.ch>
2016-04-21 17:25:35 +0200

    test file for profile e2e added

---
+++ test/e2e/test_profile.js
@@ -0,0 +1,20 @@
+/**
+ * PSIT4 FS2016 - gendok
+ *
+ * Authors: Dirk von Grünigen, Tobias Huonder, Simon Müller,
+ *          Martin Weilenmann, Aurelio Malacarne, Hannes Klauser,
+ *          Benjamin Contreras
+ *
+ */
+
+'use strict';
+
+var expect = require('chai').expect;
+var gendok = require('../../');
+var helper = require('../helper');
+var stateHelper = require('./helpers/state_helper');
+var authHelper = require('./helpers/auth_helper');
+
+describe('profile view / edit', function () {
+
+});
