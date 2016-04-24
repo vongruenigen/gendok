44b8c1ae02366daff2c0311ebf1c0e7341fad0c3
weilemar <weilemar@students.zhaw.ch>
2016-04-24 22:30:56 +0200

    add delete template tests

--- test/e2e/test_templates.js
+++ test/e2e/test_templates.js
@@ -36,11 +36,13 @@ describe('templates', function () {
   var saveButton = $('[ng-click="create()"]');
   var updateButton = $('[ng-click="update()"]');
   var editButton = $('[ng-click="edit()"]');
+  var deleteButton = $('[ng-click="delte()"]')
   var errorMessage = $('.alert-danger');
   var successMessage = $('.alert-success');
 
   beforeEach(function (done) {
     Template = gendok.data.db.getModel('Template');
+    expect(Template.truncate()).to.eventually.be.truthy;
 
     authHelper.signout(function () {
       factory.create('User', function (err, u) {
@@ -64,7 +66,6 @@ describe('templates', function () {
   describe('#/templates/create', function () {
     describe('when a valid input is given', function () {
       it('creates a template', function () {
-        expect(Template.truncate()).to.eventually.be.truthy;
 
         stateHelper.go('templateCreate');
 
@@ -117,7 +118,7 @@ describe('templates', function () {
         updateButton.click();
 
         browser.waitForAngular().then(function () {
-          expect(stateHelper.current()).to.eventually.eql('templateViewUpdate');
+         expect(stateHelper.current()).to.eventually.eql('templateViewUpdate');
           expect(successMessage.getInnerHtml()).to.eventually.eql(
            'Template new ' + tmpl.name + ' successfully updated!'
           );
@@ -140,7 +141,7 @@ describe('templates', function () {
         updateButton.click();
 
         browser.waitForAngular().then(function () {
-          expect(stateHelper.current()).to.eventually.eql('templateViewUpdate');
+         expect(stateHelper.current()).to.eventually.eql('templateViewUpdate');
           expect(errorMessage.getInnerHtml()).to.eventually.eql(
            'An error occured while updating the template.'
           );
@@ -153,7 +154,10 @@ describe('templates', function () {
     describe('when a valid id is given', function () {
       it('displays the template', function () {
         tmpl.save(function () {
-          stateHelper.go('templatesList', {templateId: tmpl.id});
+          stateHelper.go('templatesList');
+
+          var link = list.first().all(by.css('a'));
+          link.click();
 
           browser.waitForAngular();
 
@@ -172,33 +176,40 @@ describe('templates', function () {
     describe('when an invalid id is given', function () {
       it('displays the 404 page', function () {
         browser.get('#templates/98329');
+
         browser.waitForAngular();
+
         expect(stateHelper.current()).to.eventually.eql('notFound');
       });
     });
   });
 
-  describe('GET #/templates', function () {
-    describe('when a user is signed in', function () {
-      it('displays the template list', function () {
-        tmpl.save(function () {
-          stateHelper.go('templatesList');
+  describe('DELETE #/templates', function () {
+    describe('when a valid id is given', function () {
+      it('deletes a template', function () {
 
-          expect(list.count()).toEqual(1);
-          expect(list.first().first().getInnerHtml()).to.eventually.eql(tmpl.id);
+        stateHelper.go('templatesList');
+
+        var link = list.first().all(by.css('button'));
+        link.click();
+
+        browser.switchTo().alert().accept();
+
+        browser.waitForAngular().then(function () {
+          expect(stateHelper.current()).to.eventually.eql('templatesList');
+          expect(successMessage.getInnerHtml()).to.eventually.eql(
+           'The template was successfully deleted!'
+          );
+          expect(Template.count()).to.eventually.eql(0);
         });
       });
     });
 
-    describe('when no user is signed in', function () {
-      it('displays the sign in page', function () {
-        authHelper.signout(function () {
-          browser.get('#templates');
-          browser.waitForAngular();
-
-          expect(stateHelper.current()).to.eventually.eql('signin');
-        });
+    describe('when invalid id is given', function () {
+      it('displays errors', function () {
+        stateHelper.go('templatesList');
       });
     });
   });
+
 });
