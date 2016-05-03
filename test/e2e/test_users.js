9115675c04223a328e0d055cd1d837dc047ac23d
contrben <contrben@students.zhaw.ch>
2016-05-03 08:28:03 +0200

    empty e2e test-file for users added

---
+++ test/e2e/test_users.js
@@ -0,0 +1,21 @@
+/**
+ * PSIT4 FS2016 - gendok
+ *
+ * Authors: Dirk von Grünigen, Tobias Huonder, Simon Müller,
+ *          Martin Weilenmann, Aurelio Malacarne, Hannes Klauser,
+ *          Benjamin Contreras
+ *
+ */
+
+'use strict';
+
+var expect = require('chai').expect;
+var gendok = require('../../');
+var helper = require('../helper');
+var stateHelper = require('./helpers/state_helper');
+var authHelper = require('./helpers/auth_helper');
+var format = require('util').format;
+
+describe('users', function () {
+
+});
